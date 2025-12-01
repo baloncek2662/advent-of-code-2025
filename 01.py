004391a691aad2c70a92e6fa58b6b595f67285db
Gasper Martic <martic.gasper@gmail.com>
2025-12-01 15:57:37 +0100

    Add solutions for day 1
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- 01.py
+++ 01.py
@@ -2,19 +2,64 @@ from adventofcode import AoC
 
 
 def part1(inp: str) -> str | int | None:
-    return None
+    pos = 50
+    zeroes_count = 0
+    for line in inp.splitlines():
+        direction = line[:1]
+        value = int(line[1:])
+        remainder = value % 100
+        if direction == "R":
+            pos += remainder
+            pos = pos % 100
+        else:
+            pos -= remainder
+            if pos < 0:
+                pos = 100 + pos
+        if pos == 0:
+            zeroes_count += 1
+    return zeroes_count
 
 
 def part2(inp: str) -> str | int | None:
-    return None
+    pos = 50
+    zeroes_count = 0
+    for line in inp.splitlines():
+        direction = line[:1]
+        value = int(line[1:])
+        remainder = value % 100
+        zeroes_count += value // 100
+        start_pos = pos
+        if direction == "R":
+            pos += remainder
+            if pos // 100 > 0 and pos % 100 != 0 and start_pos != 0:
+                zeroes_count += 1
+            pos = pos % 100
+        else:
+            pos -= remainder
+            if pos < 0:
+                pos = 100 + pos
+                if pos != 0 and start_pos != 0:
+                    zeroes_count += 1
+        if pos == 0:
+            zeroes_count += 1
+    return zeroes_count
 
 
 aoc = AoC(part_1=part1, part_2=part2)
-inp = """sample input"""
-expected_result = None
+inp = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+expected_result = 3
 aoc.assert_p1(inp, expected_result)
 aoc.submit_p1()
 
-expected_result = None
+expected_result = 6
 aoc.assert_p2(inp, expected_result)
 aoc.submit_p2()
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # advent-of-code-2025
 Python solutions to the 2025 edition of Advent of Code
+
+## Benchmark Results
+
+<!-- BENCHMARK_RESULTS_START -->
+| Day | Status | Part 1 Time | Part 2 Time | Total Time | 
+|:---:|:------:|------------:|------------:|-----------:| 
+| 01 | ✅ | 15.0ms 🟢 | 5.1ms 🟢 | 20.2ms 🟢 |
+| 02 | 🕑 | - | - | - |
+| 03 | 🕑 | - | - | - |
+| 04 | 🕑 | - | - | - |
+| 05 | 🕑 | - | - | - |
+| 06 | 🕑 | - | - | - |
+| 07 | 🕑 | - | - | - |
+| 08 | 🕑 | - | - | - |
+| 09 | 🕑 | - | - | - |
+| 10 | 🕑 | - | - | - |
+| 11 | 🕑 | - | - | - |
+| 12 | 🕑 | - | - | - |
+| **Total** | | 15.0ms 🟢 | 5.1ms 🟢 | 20.2ms 🟢 |
+
+Legend:
+ * 🟢 < 100ms
+ * 🟡 100ms - 1s
+ * 🔴 > 1s
+<!-- BENCHMARK_RESULTS_END -->
