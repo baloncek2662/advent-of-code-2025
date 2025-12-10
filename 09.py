18fd05b28ccb54d1c02ad9021443528cf5f7fe51
Gasper Martic <martic.gasper@gmail.com>
2025-12-10 17:30:24 +0100

    Add solutions for days 9, 10 part 1
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- 09.py
+++ 09.py
@@ -1,20 +1,124 @@
 from adventofcode import AoC
 
+CANDIDATES = []
 
 def part1(inp: str) -> str | int | None:
-    return None
+    coordinates = []
+    for line in inp.splitlines():
+        coordinates.append(tuple([int(el) for el in line.split(",")]))
+    max_area = 0
+    for i, el in enumerate(coordinates):
+        for j, el_2 in enumerate(coordinates, start=i + 1):
+            diff = (abs(el[0] - el_2[0]) + 1, abs(el[1] - el_2[1]) + 1)
+            area = diff[0] * diff[1]
+            CANDIDATES.append((el, el_2, area))
+            if area > max_area:
+                max_area = area
+    return max_area
+
+
+def area_valid(el, el_2, valid_coordinates):
+    from_x = min(el[1], el_2[1])
+    to_x = max(el[1], el_2[1])
+
+    from_y = min(el[0], el_2[0])
+    to_y = max(el[0], el_2[0])
+    # print(el, el_2)
+    for i in range(from_x, to_x + 1):
+        for j in range(from_y, to_y + 1):
+            if (j, i) not in valid_coordinates:
+                # print(j,i,"not in valid coordinates")
+                return False
+    return True
+
 
 
 def part2(inp: str) -> str | int | None:
-    return None
+    coordinates = []
+    for line in inp.splitlines():
+        coordinates.append(tuple([int(el) for el in line.split(",")]))
+    max_area = 0
+    valid_coordinates = set()
+    for i, el in enumerate(coordinates):
+        # valid_coordinates.add(el)
+        for j, el_2 in enumerate(coordinates, start=i + 1):
+            # if there is an # to the right or to the bottom of this coordinate,
+            # mark all coordinates up to it as valid
+            if el[0] == el_2[0]:
+                from_x = min(el[1], el_2[1])
+                to_x = max(el[1], el_2[1])
+                for x in range(from_x, to_x + 1):
+                    valid_coordinates.add((el[0], x))
+
+            if el[1] == el_2[1]:
+                from_y = min(el[0], el_2[0])
+                to_y = max(el[0], el_2[0])
+                for y in range(from_y, to_y + 1):
+                    valid_coordinates.add((y, el[1]))
+
+    # print(valid_coordinates)
+
+    # edge_coordinates = set()
+    # print(valid_coordinates)
+    # for el in valid_coordinates:
+    #     # el is edge coordinate if not all its surrounding elements are in valid_voordinages
+    #     print(el, (el[0], el[1] - 1) in valid_coordinates , (el[0] - 1, el[1]) in valid_coordinates , (el[0], el[1] + 1) in valid_coordinates , (el[0] + 1, el[1]) in valid_coordinates)
+    #     if not ((el[0], el[1] - 1) in valid_coordinates and (el[0] - 1, el[1]) in valid_coordinates and (el[0], el[1] + 1) in valid_coordinates and (el[0] + 1, el[1]) in valid_coordinates):
+    #         edge_coordinates.add(el)
+        # if (el[0], el[1] + 1) not in valid_coordinates and (el[0] - 1, el[1]) not in valid_coordinates:
+        #     edge_coordinates.add(el)
+        # if (el[0], el[1] + 1) not in valid_coordinates and (el[0] + 1, el[1]) not in valid_coordinates:
+        #     edge_coordinates.add(el)
+        # if (el[0], el[1] - 1) not in valid_coordinates and (el[0] + 1, el[1]) not in valid_coordinates:
+        #     edge_coordinates.add(el)
+    print(len(valid_coordinates))
+    # print(edge_coordinates)
+
+    # valid_coordinates_2 = set()
+    # for i, el in enumerate(valid_coordinates):
+    #     for j, el_2 in enumerate(valid_coordinates, start=i + 1):
+    #         # if there is an # to the right or to the bottom of this coordinate,
+    #         # mark all coordinates up to it as valid
+    #         if el[0] == el_2[0]:
+    #             from_x = min(el[1], el_2[1])
+    #             to_x = max(el[1], el_2[1])
+    #             for x in range(from_x, to_x + 1):
+    #                 valid_coordinates_2.add((el[0], x))
+
+    #         if el[1] == el_2[1]:
+    #             from_y = min(el[0], el_2[0])
+    #             to_y = max(el[0], el_2[0])
+    #             for y in range(from_y, to_y + 1):
+    #                 valid_coordinates_2.add((y, el[1]))
+    # print(valid_coordinates_2)
+
+    # use same solution as part 1, but check that all coordinates in the area are in valid_coordinates_2
+    max_area = 0
+    for i, el in enumerate(coordinates):
+        for j, el_2 in enumerate(coordinates, start=i + 1):
+            if not area_valid(el, el_2, valid_coordinates):
+                continue
+            # print("contains valid area!", el, el_2)
+            diff = (abs(el[0] - el_2[0]) + 1, abs(el[1] - el_2[1]) + 1)
+            area = diff[0] * diff[1]
+            if area > max_area:
+                max_area = area
+    return max_area
 
 
 aoc = AoC(part_1=part1, part_2=part2)
-inp = """sample input"""
-expected_result = None
+inp = """7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3"""
+expected_result = 50
 aoc.assert_p1(inp, expected_result)
 aoc.submit_p1()
 
-expected_result = None
-aoc.assert_p2(inp, expected_result)
+# expected_result = 24
+# aoc.assert_p2(inp, expected_result)
 aoc.submit_p2()
--- 10.py
+++ 10.py
@@ -1,8 +1,62 @@
 from adventofcode import AoC
+import re
+import copy
+from collections import deque
+
+
+def is_diagram_after_moves_ok(diag: list[bool], moves):
+    # start with state where all lights are off
+    state = []
+    for _ in range(len(diag)):
+        state.append(False)
+
+    for m in moves:
+        state[m] = not state[m]
+
+    if state == diag:
+        print("winning moves:", moves)
+        return True
+    return False
+
+
+def get_min_presses(diag, buttons):
+    queue = deque()
+    for i, b in enumerate(buttons):
+        queue.append({
+            "moves": b,
+            "count": 1,
+            "pressed_ixs": [i]
+        })
+
+    while True:
+        el = queue.popleft()
+        if is_diagram_after_moves_ok(diag, el["moves"]):
+            return el["count"]
+        for i, b in enumerate(buttons):
+            if i not in el["pressed_ixs"]:
+                new_moves = copy.deepcopy(el["moves"]) + b
+                new_pressed_ixs = copy.deepcopy(el["pressed_ixs"]) + [i]
+                queue.append({
+                    "moves": new_moves,
+                    "count": el["count"] + 1,
+                    "pressed_ixs": new_pressed_ixs,
+                })
 
 
 def part1(inp: str) -> str | int | None:
-    return None
+    manuals = []
+    for line in inp.splitlines():
+        res = {}
+        lights = re.search(r"\[(.*)\]", line)
+        res["diag"] = [l == "#" for l in lights.group()[1:-1]]
+        buttons = re.findall(r"\((.*?)\)", line)
+        res["buttons"] = [[int(b) for b in button.split(",")] for button in buttons]
+        manuals.append(res)
+    total = 0
+    for m in manuals:
+        # min_presses = press(m["diag"], m["buttons"], None, set())
+        total += get_min_presses(m["diag"], m["buttons"])
+    return total
 
 
 def part2(inp: str) -> str | int | None:
@@ -10,8 +64,10 @@ def part2(inp: str) -> str | int | None:
 
 
 aoc = AoC(part_1=part1, part_2=part2)
-inp = """sample input"""
-expected_result = None
+inp = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+expected_result = 7
 aoc.assert_p1(inp, expected_result)
 aoc.submit_p1()
 
